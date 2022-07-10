d37f7d5e94b4963af8bfcef3fce4207c93454e7b
nothingprogram <popawaw@naver.com>
2022-07-10 15:40:40 +0900

    Spring boot unit testing - prototype beans

--- src/test/java/io/jongyun/springbootwithtdd/SpringBootWithTddApplicationTests.java
+++ src/test/java/io/jongyun/springbootwithtdd/SpringBootWithTddApplicationTests.java
@@ -1,9 +1,12 @@
 package io.jongyun.springbootwithtdd;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.jongyun.springbootwithtdd.models.CollegeStudent;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 // package 명과 test 폴더 명이 다를 경우 명시적으로 classes 를 적어준다.
 @SpringBootTest(classes = SpringBootWithTddApplication.class)
@@ -24,6 +28,7 @@ class SpringBootWithTddApplicationTests {
   private static int count = 0;
   @Autowired CollegeStudent student;
   @Autowired StudentGrades studentGrades;
+  @Autowired ApplicationContext context;
 
   @Value("${info.app.name}")
   private String appInfo;
@@ -92,5 +97,45 @@ class SpringBootWithTddApplicationTests {
     assertNotNull(
         studentGrades.checkNull(student.getStudentGrades().getMathGradeResults()),
         "object should not be null");
+  }
+
+  @DisplayName("Create student without grade init")
+  @Test
+  public void createStudentWithoutGradeInit() {
+    CollegeStudent studentTwo = context.getBean("collegeStudent", CollegeStudent.class);
+
+    studentTwo.setFirstname("Ha");
+    studentTwo.setLastname("Jongyun");
+    studentTwo.setEmailAddress("gkwhddbs9999@gmail.com");
+
+    assertNotNull(studentTwo.getFirstname());
+    assertNotNull(studentTwo.getLastname());
+    assertNotNull(studentTwo.getEmailAddress());
+    assertNull(studentGrades.checkNull(studentTwo.getStudentGrades()));
+  }
+
+  @DisplayName("Verify students are prototypes")
+  @Test
+  public void verifyStudentsArePrototypes() {
+    CollegeStudent studentTwo = context.getBean("collegeStudent", CollegeStudent.class);
+
+    assertNotSame(student, studentTwo);
+  }
+
+  @DisplayName("Find Grade Point Average")
+  @Test
+  public void findGradePointAverage() {
+    assertAll(
+        "Testing all assertEquals",
+        () ->
+            assertEquals(
+                353.25,
+                studentGrades.addGradeResultsForSingleClass(
+                    student.getStudentGrades().getMathGradeResults())),
+        () ->
+            assertEquals(
+                88.31,
+                studentGrades.findGradePointAverage(
+                    student.getStudentGrades().getMathGradeResults())));
   }
 }

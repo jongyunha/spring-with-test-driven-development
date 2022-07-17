d62c8606df499d911ae8ceff5f8d90007b1e1003
nothingprogram <popawaw@naver.com>
2022-07-17 22:38:29 +0900

    Mocking with mockito part3

--- src/test/java/io/jongyun/springbootwithtdd/MockAnnotationTest.java
+++ src/test/java/io/jongyun/springbootwithtdd/MockAnnotationTest.java
@@ -1,10 +1,17 @@
 package io.jongyun.springbootwithtdd;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import io.jongyun.springbootwithtdd.dao.v2.ApplicationDaoV2;
 import io.jongyun.springbootwithtdd.models.CollegeStudent;
 import io.jongyun.springbootwithtdd.models.StudentGrades;
 import io.jongyun.springbootwithtdd.service.ApplicationServiceV2;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +45,23 @@ public class MockAnnotationTest {
         studentOne.setFirstname("Ha");
         studentOne.setEmailAddress("popawaw@naver.com");
         studentOne.setStudentGrades(studentGrades);
+    }
+
+    @DisplayName("When & Verify")
+    @Test
+    void assertEqualsTestAddGrades() {
+        when(applicationDaoV2.addGradeResultsForSingleClass(studentGrades.getMathGradeResults()))
+            .thenReturn(100.0);
+
+        assertEquals(100, applicationServiceV2
+            .addGradeResultsForSingleClass(studentOne.getStudentGrades().getMathGradeResults()));
+
+        // 이 method 가 호출되었는지 검증
+        verify(applicationDaoV2).addGradeResultsForSingleClass(studentGrades.getMathGradeResults());
+
+        // 이 method 가 몇번 호출되었는지 검증
+        verify(applicationDaoV2, times(1)).addGradeResultsForSingleClass(
+            studentGrades.getMathGradeResults());
     }
 
 }
